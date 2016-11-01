9a7e5d2463f32ff4b333532ca28a0e20ddbc2d88
aselvais <arnaud@antidot.ca>
2016-10-31 20:53:22 -0400

    Update and cleanup the code.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# nodeimageleecher
+# Node Image Leecher
 Simple and messy node script for downloading all the images from a webpage
+
+## Installation
+
+Install the dependencies:
+
+	npm install
+
+Make the directory what will contain the images files we will download:
+
+	mkdir downloaded
+
+
+## Usage
+
+	node index.js website_url
+	
+Where "website_url" is the url of the page we want to download images from.
--- index.js
+++ index.js
@@ -1,47 +1,87 @@
 var http = require('http');
-var $ = require('jquery');
 var jsdom = require("jsdom");
 var request = require('request');
-var http = require('http'),
-    Stream = require('stream').Transform,
-    fs = require('fs');
+var http = require('http');
+var fs = require('fs');
 var sanitize = require("sanitize-filename");
+var url = '';
 
-var url = process.argv[2];
-
-var download = function(uri, filename, callback){
-  console.log('Downloading '+uri);
-  request.head(uri, function(err, res, body){
-    try {
-      request(uri).on('error', function(err) {
-    console.log(err);
-  }).pipe(fs.createWriteStream(filename)).on('close', callback);
-    } catch (errr) {
-      console.log(errr);
-      callback.apply();
-    }
-  });
-};
-
-jsdom.env(
-  url,
-  ["http://code.jquery.com/jquery.js"],
-  function (err, window) {
-    var $ = require("jquery")(window);
-    var files = [];
-    $('a').each(function(e){
-      var ll = $(this).attr('href')
-      if (ll.match(/\.jpg$/)) {
-        files.push(ll);
-      }
-    });
-for (var i = files.length-1; i > 0; i--)
+if (process && process.argv && process.argv[2])
 {
-     download(url + files[i], sanitize(files[i]), function(){
-       console.log(files[i] + ' done');
-     });
+	url = process.argv[2];
+}
+else
+{
+	console.log('an URL is missing ...');
+	process.exit();
 }
 
+/**
+ *
+ * @param uri
+ * @param filename
+ * @param callback
+ */
+var download = function (uri, filename, callback)
+{
+	console.log('Downloading ' + uri);
+	request.head(uri, function (err, res, body)
+	{
+		try
+		{
+			request(uri).on('error', function (err)
+			{
+				console.log(err);
+			}).pipe(fs.createWriteStream('./downloaded/' + filename)).on('close', callback);
+		} catch (errr)
+		{
+			console.log(errr);
+			callback.apply();
+		}
+	});
+};
 
-  }
+/**
+ *
+ * @param err
+ * @param window
+ */
+var getImageFromPage = function (err, window)
+{
+	var $ = require("jquery")(window);
+
+	$('img').add('a').each(function (e)
+	{
+		var ll = '';
+		if ($(this).attr('href'))
+		{
+			ll = $(this).attr('href');
+		}
+		if ($(this).attr('src'))
+		{
+			ll = $(this).attr('src');
+		}
+		if (ll.match(/\.jpg$|\.png|\.gif/))
+		{
+			var fullUrl = '';
+			if (fullUrl.match(/^http/))
+			{
+				fullUrl = ll;
+			} else
+			{
+				fullUrl = url + ll;
+			}
+			download(fullUrl + '', sanitize(ll + ''), function ()
+			{
+				console.log(ll + ' done');
+			});
+		}
+	});
+};
+
+console.log('Getting images from ' + url + ' ...');
+jsdom.env(
+	url,
+//	["http://code.jquery.com/jquery.js"],
+	getImageFromPage
 );
